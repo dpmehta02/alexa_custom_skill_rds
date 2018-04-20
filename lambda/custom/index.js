b70b2ac630405b7eebf4f484cd0e628105b0d23b
Dev M <dpmehta02@gmail.com>
2018-04-19 19:10:52 -0700

    Add linting

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,24 +8,38 @@ const LaunchRequestHandler = {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+    // Query DB by userID for chains.
+
+    const cardText;
+    const speechText;
+    if (true) { // If no userID or empty chains
+      // TODO: Tell user to check alexa app for link to create chains (return card w/ link to companion app).
+      speechText = '';
+      cardText = 'http://www.companionapp.io';
+    } else { // If does, ask which chain to trigger. Your current chains are ..., ..., .
+      // TODO: Return custom audio in place of this and move this string to cardText
+      speechText = 'Welcome to Daisy Chains! Which chain would you like to trigger? Your choices are ... .';
+      cardText = speechText;
+    }
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .withSimpleCard('Daisy Chains', cardText) // https://developer.amazon.com/docs/custom-skills/include-a-card-in-your-skills-response.html#overview-of-cards
       .getResponse();
   },
 };
 
+// Modify handler to accept utterance
 const TriggerChainIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'TriggerChainIntent';
   },
   handle(handlerInput) {
-    // Update logic.
     const speechText = 'Hello World!';
+    // If no matching chain, list current chains and await response -- GET API
+    // Else trigger chain based on utterance (and say 'success!') -- hit API
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -57,7 +71,7 @@ const CancelAndStopIntentHandler = {
         || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
-    const speechText = 'Goodbye!';
+    const speechText = 'Peace!';
 
     return handlerInput.responseBuilder
       .speak(speechText)
